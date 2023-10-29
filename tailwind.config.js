d0bd023ba13881aacc17647b5b6f087a5665875c
sphere <sphere.stacking@gmail.com>
2023-10-29 14:52:41 +0900

    テーマ変更を行えるように設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,8 @@ export default {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ['light', 'dark', 'cupcake'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'light', // name of one of the included themes for dark mode
+    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'cupcake', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
