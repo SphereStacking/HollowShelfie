a95b2d99b841dd1d595213d49167c732716d5a55
sphere <sphere.stacking@gmail.com>
2023-11-07 00:11:09 +0900

    userというルートをaccountに変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,14 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/fortify.php'));
+                
+            Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/jetstream.php'));
         });
     }
 }
