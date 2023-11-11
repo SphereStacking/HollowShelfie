ecefa50d546a7d92b2059a1081737d22318b103b
sphere <sphere.stacking@gmail.com>
2023-11-11 22:36:13 +0900

    認証エラーの画面を作成

---
+++ resources/js/Pages/Errors/InvalidState.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center px-4 py-8">
+    <div class="max-w-md w-full space-y-8 flex flex-col">
+      <h1 class="text-3xl font-bold mb-4 text-center">認証エラー</h1>
+      <p class="text-lg text-gray-700 text-center">認証プロセス中に問題が発生しました。もう一度お試しください。</p>
+      <Link href="/" class="ml-4 btn">
+      戻る
+      </Link>
+    </div>
+  </div>
+</template>
+
