042e8e0d94dbf7e62026a561011317e1bfb30b98
SphereStacking <sphere.stacking@gmail.com>
2023-11-13 18:55:17 +0900

    ダッシュボード及びWelwcomページを削除してHomeに変更

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -66,9 +66,9 @@ const submit = () => {
             <Checkbox id="terms" v-model:checked="form.terms" name="terms" required />
 
             <div class="ml-2">
-              I agree to the <a target="_blank" :href="route('terms.show')"
+              I agree to the <a target="_blank" :href="route('legal', 'terms-of-service')"
                 class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Terms
-                of Service</a> and <a target="_blank" :href="route('policy.show')"
+                of Service</a> and <a target="_blank" :href="route('legal', 'privacy-policy')"
                 class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Privacy
                 Policy</a>
             </div>
