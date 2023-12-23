1185c2353aca2c8324eb57eb8e903f5c84de3043
sphere <sphere.stacking@gmail.com>
2023-12-24 02:26:30 +0900

    service修正

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,23 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Services\TeamService;
 use App\Services\UserService;
+use App\Params\EventSearchParams;
+use App\Services\EventMeilisearchService;
+use App\Http\Resources\EventListJsonResource;
 use App\Http\Resources\UserPublicProfileJsonResource;
 
 class ProfileController extends Controller
 {
     protected $userService;
     protected $teamService;
+    protected $eventMeilisearchService;
 
     public function __construct(
         UserService $userService,
         TeamService $teamService,
+        EventMeilisearchService  $eventMeilisearchService
     ) {
         $this->userService = $userService;
         $this->teamService = $teamService;
+        $this->eventMeilisearchService = $eventMeilisearchService;
     }
 
     //
@@ -28,14 +35,46 @@ class ProfileController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\View\View
      */
-    public function userProfileShow($id)
+    public function user($id)
     {
         // User モデルのルートモデルバインディングを使用してユーザーを取得
         // ユーザープロファイルのビューを返す
+        $user = User::find($id);
+        $EventSearchParams = new EventSearchParams(
+            '',
+            [['include' => 'and', 'type' => 'user', 'value' => $user->name]],
+            12,
+            'new',
+        );
+
         return Inertia::render('User/Index', [
             'profile' => new UserPublicProfileJsonResource(
                 $this->userService->getPublishProfile($id),
             ),
+            'events' => new EventListJsonResource(
+                $this->eventMeilisearchService->getPublishedEventSearch($EventSearchParams)
+            ),
+            'url' => route('event.search.index', [
+                't' => $EventSearchParams->text,
+                'q' => $EventSearchParams->queryParams,
+                'paginate' => $EventSearchParams->getDefaultPaginate(),
+                'o' => $EventSearchParams->order,
+            ])
+        ]);
+    }
+
+    /**
+     * チームのプロファイルを表示します。
+     *
+     * @param  \App\Models\Team  $team
+     * @return \Illuminate\View\View
+     */
+    public function team($id)
+    {
+        // Team モデルのルートモデルバインディングを使用してチームを取得
+        // チームプロファイルのビューを返す
+        return Inertia::render('Team/Index', [
+            'profile' => $this->teamService->getPublishProfile($id)
         ]);
     }
 
@@ -45,20 +84,6 @@ class ProfileController extends Controller
             'adminUser' => new UserPublicProfileJsonResource(
                 $this->userService->getPublishProfile(1),
             ),
-        ]);
-    }
-    /**
-     * チームのプロファイルを表示します。
-     *
-     * @param  \App\Models\Team  $team
-     * @return \Illuminate\View\View
-     */
-    public function teamProfileShow($id)
-    {
-        // Team モデルのルートモデルバインディングを使用してチームを取得
-        // チームプロファイルのビューを返す
-        return Inertia::render('Team/Index', [
-            'profile' => $this->teamService->getPublishProfile($id)
         ]);
     }
 }
