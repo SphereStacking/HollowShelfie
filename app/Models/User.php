5c34ea429ff035e75dbb3c25de61db9fd4c84ba4
SphereStacking <sphere.stacking@gmail.com>
2023-12-24 02:31:56 +0900

    badge用のrelationを追加し定義をtraitsに移動

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Badge;
 use App\Models\SocialAccount;
 use Laravel\Jetstream\HasTeams;
 use App\Traits\UserProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Traits\UserRelations;
 use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -21,6 +23,8 @@ class User extends Authenticatable
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+    use UserRelations;
 
     /**
      * The attributes that are mass assignable.
@@ -58,7 +62,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $appends = [
-        'profile_photo_url', 'links'
+        'profile_photo_url', 'links',
     ];
 
     /**
@@ -69,67 +73,5 @@ class User extends Authenticatable
     public function getLinksAttribute()
     {
         return $this->links()->get();
-    }
-
-    //外部認証アカウントリレーション
-    public function social_accounts()
-    {
-        return $this->hasMany(SocialAccount::class);
-    }
-
-    //
-    public function bookmark_events()
-    {
-        return $this->belongsToMany(Event::class, 'event_user_bookmark');
-    }
-
-    // GroupModel が複数のイベントを主催している場合
-    public function good_events()
-    {
-        return $this->belongsToMany(Event::class, 'event_user_good');
-    }
-
-    /**
-     * このUserがオーガナイザーしているイベントを取得
-     */
-    public function event_organizers()
-    {
-        return $this->morphMany(EventOrganizer::class, 'event_organizeble');
-    }
-
-
-    //リンク
-    public function links()
-    {
-        return $this->morphMany(Link::class, 'linkable');
-    }
-
-    //フォロー機能 Userがフォローしている人
-    public function follows()
-    {
-        return $this->morphToMany(User::class, 'followable', 'follows', 'user_id', 'followable_id');
-    }
-
-    //フォロー機能 Userをfollowしている人
-    public function followers()
-    {
-        return $this->morphToMany(User::class, 'followable', 'follows')->withTimestamps();
-    }
-    // ユーザーのフォロワー数を取得する
-    public function followersCount()
-    {
-        return $this->followers()->count();
-    }
-
-    // このユーザーがフォローしている人数を取得する
-    public function followingsCount()
-    {
-        return $this->followings()->count();
-    }
-
-    // 認証してるユーザーがフォローしているがどうか
-    public function isFollowed()
-    {
-        return $this->followers()->where('user_id', auth()->id())->exists();
     }
 }
