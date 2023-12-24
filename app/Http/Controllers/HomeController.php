8e2f543a15cb470013e198cbc748f389d7e96468
SphereStacking <sphere.stacking@gmail.com>
2023-12-24 23:54:32 +0900

    refactor: イベント表示機能のHome.vueへの移行とリファクタリング
    この差分は、Event/Index.vueとHome.vueの2つのVueファイルと、EventController.phpとHomeController.phpの2つのPHPファイルに関連しています。
    
    Event/Index.vueの変更点:
    - 画像のインポートとそれらを配列に格納するコードが削除されました。
    - propsの定義から、各種イベントの配列が削除されました。
    - getEvetnListSection関数が削除されました。
    - テンプレート部分から、各種イベントの表示に関するコードが削除され、代わりに探すと開催するというテキストが追加されました。
    
    Home.vueの変更点:
    - 画像のインポートとそれらを配列に格納するコードが追加されました。
    - propsの定義に、各種イベントの配列が追加されました。
    - getEvetnListSection関数が追加されました。
    - テンプレート部分に、各種イベントの表示に関するコードが追加されました。
    
    EventController.phpの変更点:
    - EventTagServiceのプロパティが削除されました。
    
    HomeController.phpの変更点:
    - EventServiceのプロパティとコンストラクタが追加されました。
    - indexメソッドが更新され、各種イベントのデータを取得してHomeビューに渡すようになりました。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Services\EventService;
+use App\Http\Resources\EventsJsonResource;
 
 class HomeController extends Controller
 {
+    protected $eventService;
+
+    public function __construct(
+        EventService $eventService,
+    ) {
+        $this->eventService = $eventService;
+    }
+
     public function index()
     {
-        return Inertia::render('Home');
+        return Inertia::render(
+            'Home',
+            [
+                'newEvents' => new EventsJsonResource($this->eventService->getSectionedQuery('new')->take(12)->get()),
+                'ongoingEvents' => new EventsJsonResource($this->eventService->getSectionedQuery('ongoing')->take(12)->get()),
+                'highlightEvents' => new EventsJsonResource($this->eventService->getSectionedQuery('highlight')->take(12)->get()),
+                'recentEvents' => new EventsJsonResource($this->eventService->getSectionedQuery('recent')->take(12)->get()),
+                'myBookmarkEvents' => new EventsJsonResource($this->eventService->getSectionedQuery('mybookmark')->take(12)->get()),
+            ]
+        );
     }
 }
