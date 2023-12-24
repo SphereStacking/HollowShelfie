b60089e0af700cf32eac068ba8d450a887f3df54
sphere <sphere.stacking@gmail.com>
2023-12-24 16:44:48 +0900

    refactor: 共通設定を使用するように修正しコンポーネント名を適切なものに変更

---
+++ resources/js/Components/BtnConditionTypeMapper.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ItemsConfig } from '@/config';
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: true
+  },
+});
+
+const getButtonClassForType = (type) => ItemsConfig[type]?.buttonClass || ItemsConfig.default.buttonClass;
+</script>
+
+<template>
+  <button class="btn btn-xs rounded-md cursor-pointer inline-flex items-center text-sm font-medium group"
+    :class="getButtonClassForType(type)">
+    <slot></slot>
+  </button>
+</template>
