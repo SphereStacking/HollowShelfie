97971a23875cedd38d3221c87b773cbd2e0b9454
sphere <sphere.stacking@gmail.com>
2023-12-30 08:29:06 +0900

    仮配置

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -58,9 +58,24 @@ const urlSetter = (event) => {
 getButtonText
 
 const eventItems = ref([])
-eventItems.value.push({ url: props.newEventsUrl, events: props.newEvents, title: 'NEW', icon: 'mdi:new-box' })
 eventItems.value.push({ url: props.ongoingEventsUrl, events: props.ongoingEvents, title: 'OPEN', icon: 'mdi:door-open' })
 eventItems.value.push({ url: props.recentEventsUrl, events: props.recentEvents, title: 'Recent', icon: 'mdi:timelapse' })
+eventItems.value.push({ url: props.newEventsUrl, events: props.newEvents, title: 'NEW', icon: 'mdi:new-box' })
+
+import image1 from '@/tmp/2024SummerFestival.png'
+import image2 from '@/tmp/Comiket.png'
+import image3 from '@/tmp/SuccubusCafe.png'
+import image4 from '@/tmp/VOCALOIDFuture.png'
+import image5 from '@/tmp/Werewolf.png'
+
+//TODO:仮
+const slides = [
+  { image: image1, url: 'http://localhost/event/1/show' },
+  { image: image2, url: 'http://localhost/event/2/show' },
+  { image: image3, url: 'http://localhost/event/3/show' },
+  { image: image4, url: 'http://localhost/event/4/show' },
+  { image: image5, url: 'http://localhost/event/5/show' },
+]
 
 </script>
 
@@ -73,10 +88,10 @@ eventItems.value.push({ url: props.recentEventsUrl, events: props.recentEvents, 
     </template>
 
     <div class="mx-auto mt-5 flex max-w-7xl flex-col gap-10">
-      <CarouselEventHero :events="trendEvents"></CarouselEventHero>
+      <CarouselBanner :slides="slides"></CarouselBanner>
 
       <div class="flex flex-wrap gap-2 justify-center">
-        <div class="divider divider-neutral divider-start my-10 w-full text-3xl font-bold">
+        <div class="divider divider-neutral divider-start mt-5 w-full text-3xl font-bold">
           <IconTypeMapper type="category"></IconTypeMapper>
           Category
         </div>
@@ -85,7 +100,7 @@ eventItems.value.push({ url: props.recentEventsUrl, events: props.recentEvents, 
         </BtnEventSerchItem>
       </div>
       <div class="flex flex-wrap gap-2 justify-center">
-        <div class="divider divider-neutral divider-start my-10 w-full text-3xl font-bold  ">
+        <div class="divider divider-neutral divider-start mt-5 w-full text-3xl font-bold  ">
           <IconTypeMapper type="tag"></IconTypeMapper>
           tag
         </div>
@@ -95,17 +110,20 @@ eventItems.value.push({ url: props.recentEventsUrl, events: props.recentEvents, 
       </div>
 
 
-      <div class="flex flex-col " v-for="items in eventItems">
-        <div class="divider divider-neutral divider-start my-10 w-full text-3xl font-bold">
+      <div class="flex flex-col gap-5" v-for="items in eventItems">
+        <div class="divider divider-neutral divider-start mt-5 w-full text-3xl font-bold">
           <Icon :icon="items.icon"></Icon>
           <h4 class="font-bold">{{ items.title }}</h4>
         </div>
+
+        <CarouselEventHero :events="items.events"></CarouselEventHero>
 
         <div class="w-full grid xl:grid-cols-6 md:grid-cols-4  sm:grid-cols-3 grid-cols-2  gap-10 ">
           <CardEvent v-for="(event, index) in items.events" :key="index" :event="event" class=" min-h-48 " />
         </div>
         <Link class=" btn btn-neutral w-full mt-10" :href="items.url">show more!</Link>
       </div>
+
 
     </div>
   </AppLayout>
