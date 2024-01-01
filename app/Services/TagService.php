dc6183fdacacc935c4c15864eb20c165491c5a09
SphereStacking <sphere.stacking@gmail.com>
2024-01-01 10:54:49 +0900

    refactor: tagServiceにmethodを移植
    getTrendTagNamesがEventServiceにあり関連度が低かったため

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -3,6 +3,9 @@
 namespace App\Services;
 
 use App\Models\Tag;
+use App\Models\Event;
+use App\Models\Taggable;
+use Illuminate\Support\Facades\DB;
 
 class TagService
 {
@@ -12,5 +15,17 @@ class TagService
             ->orderByDesc('events_count')
             ->limit($limit)
             ->get();
+    }
+
+    public function getTrendTagNames()
+    {
+        $popularTagIds = Taggable::where('taggable_type', Event::class)
+            ->select('tag_id', DB::raw('COUNT(tag_id) as count'))
+            ->groupBy('tag_id')
+            ->orderByDesc('count')
+            ->limit(4)
+            ->pluck('tag_id');
+
+        return Tag::whereIn('id', $popularTagIds)->pluck('name');
     }
 }
