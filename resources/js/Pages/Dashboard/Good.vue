0f217cc465d202a9efc2599c25a5d43ed54c5545
sphere <sphere.stacking@gmail.com>
2024-01-14 00:27:25 +0900

    feat: Dashboard画面のフロントを作成

---
+++ resources/js/Pages/Dashboard/Good.vue
@@ -0,0 +1,68 @@
+<script setup>
+const props = defineProps({
+  events: {
+    type: Array,
+    required: true
+  },
+})
+
+const order = ref('')
+
+const orderMaps = [
+  { type: 'new', icon: 'mdi:new-box', label: 'new' },
+  { type: 'old', icon: 'mdi:alpha-o-box', label: 'old' },
+]
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight ">
+        ダッシュボード
+      </h2>
+    </template>
+    いいね
+    <div class="mx-auto flex max-w-7xl flex-col gap-5">
+      <div class="flex flex-row  justify-between">
+        <div class="">
+          {{ events.pagination.to }} / {{ events.pagination.total }}
+        </div>
+        <div class=" flex flex-row gap-2">
+          <BtnPagination
+            :prev-page-url="props.events.pagination.prev_page_url" :next-page-url="props.events.pagination.links"
+            :links="events.pagination.links" />
+          <div class="dropdown dropdown-end ">
+            <div tabindex="0" class=" btn indicator  btn-neutral btn-sm w-16 px-2">
+              <Icon icon="mdi:sort" class="text-xl transition-all" />
+              <div v-if="order.icon" class="badge indicator-item badge-info">
+                <Icon :icon="order.icon" class=" text-sm " />
+              </div>
+            </div>
+            <ul tabindex="0" class="menu dropdown-content z-[1] w-28 rounded-box bg-base-100 p-2 shadow">
+              <li v-for="(orderItem, index) in orderMaps" :key="index" @click="order = orderItem; executeSearch()">
+                <div>
+                  <Icon :icon="orderItem.icon" />
+                  {{ orderItem.label }}
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="my-2 grid w-full grid-cols-2  gap-6 sm:grid-cols-3  md:grid-cols-4 xl:grid-cols-6">
+        <CardEvent
+          v-for="(item, index) in props.events.data" :key="index" :event="item"
+          scroll-region />
+      </div>
+      <div class="flex flex-row justify-center">
+        <BtnPagination
+          :prev-page-url="props.events.pagination.prev_page_url" :next-page-url="props.events.pagination.links"
+          :links="events.pagination.links" />
+      </div>
+    </div>
+  </AppLayout>
+</template>
+
+<style lang="">
+
+</style>
---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import IconTypeMapper from '@/Components/IconTypeMapper.vue'
+const props = defineProps({
+  auth: {
+    type: Object,
+    required: true,
+  },
+  counts: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+
+const dashboardCategory = [
+  { id: 'activity', iconType: 'activity', title: 'アクティビティ' },
+  { id: 'search', iconType: 'search', title: '探索！' },
+  { id: 'socialInteraction', iconType: 'socialInteraction', title: 'Social interaction' },
+  { id: 'team', iconType: 'team', title: 'Team' },
+  { id: 'organize', iconType: 'organize', title: 'Organize' },
+  { id: 'setting', iconType: 'setting', title: 'Setting' },
+]
+const dashboardItems = [
+  { category: 'activity', iconType: 'bookmarks', title: 'bookmarks', url: route('dashboard.bookmark', props.auth.user.screen_name), buttonText: props.counts.bookmark?.total || 0 },
+  { category: 'activity', iconType: 'onGood', title: 'Goods', url: route('user.good', props.auth.user.screen_name), buttonText: props.counts.good || 0 },
+  { category: 'search', iconType: 'event', title: 'イベントを探す', url: route('event.search.index'), buttonText: 'go' },
+  { category: 'search', iconType: 'team', title: 'チームを探す(仮)', url: '#', buttonText: 'go' },
+  { category: 'search', iconType: 'user', title: 'ユーザーを探す(仮)', url: '#', buttonText: 'go' },
+  { category: 'search', iconType: 'recruiting', title: '募集を探す(仮)', url: route('event.recruiting'), buttonText: 'go' },
+  { category: 'socialInteraction', iconType: 'follow', title: 'フォロー', url: route('user.follow', props.auth.user.screen_name), buttonText: props.counts.follow || 0 },
+  { category: 'socialInteraction', iconType: 'follower', title: 'フォロワー', url: route('user.follower', props.auth.user.screen_name), buttonText: props.counts.follower || 0 },
+  { category: 'team', iconType: 'teamCreate', title: 'チームを作成する', url: route('teams.create'), buttonText: 'go!' },
+  { category: 'team', iconType: 'team', title: '所属チーム(仮)', url: '#', buttonText: 'go' },
+  { category: 'organize', iconType: 'event', title: 'イベントを主催する(仮)', url: route('event.create'), buttonText: 'go' },
+  { category: 'organize', iconType: 'myEvents', title: 'イベント管理(仮)', url: '#', buttonText: '表示' },
+  { category: 'setting', iconType: 'account', title: 'アカウント設定', url: route('profile.show'), buttonText: 'go' },
+  { category: 'setting', iconType: 'profile', title: '公開プロファイル', url: props.auth.user.profile_url, buttonText: 'go' },
+]
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight ">
+        ダッシュボード
+      </h2>
+    </template>
+
+    <div class="mx-auto grid w-full max-w-7xl grid-cols-2  gap-5 sm:grid-cols-3  md:grid-cols-4 xl:grid-cols-6">
+      <template v-for="category in dashboardCategory" :key="category.id">
+        <div class="divider divider-start col-span-full mt-10 text-xl  font-semibold">
+          <IconTypeMapper :type="category.iconType" class="text-2xl" />
+          {{ category.title }}
+        </div>
+        <template v-for="item in dashboardItems" :key="item.title">
+          <div
+            v-if="item.category === category.id"
+            class="col-span-2">
+            <Card>
+              <template #title>
+                <div class="flex w-full items-center justify-between">
+                  <div class="flex items-center gap-0.5">
+                    <IconTypeMapper :type="item.iconType" />
+                    {{ item.title }}
+                  </div>
+                  <Link class="btn btn-primary btn-sm w-20" :href="item.url">
+                    {{ item.buttonText }}
+                  </Link>
+                </div>
+              </template>
+            </Card>
+          </div>
+        </template>
+      </template>
+    </div>
+  </AppLayout>
+</template>
+
+<style lang="">
+
+</style>
+
