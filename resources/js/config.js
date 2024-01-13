d139371b266e4acc2f500f6f6e7a6c90303e2418
sphere <sphere.stacking@gmail.com>
2024-01-14 01:25:31 +0900

    feat: 共通的なiconなどの設定を更新

--- resources/js/config.js
+++ resources/js/config.js
@@ -31,8 +31,89 @@ export const ItemsConfig = {
     icon: 'mdi:dots-hexagon',
     buttonClass: 'btn-info'
   },
+  bookmarks: {
+    icon: 'mdi:bookmark-multiple',
+    buttonClass: ''
+  },
+  onBookmarks: {
+    icon: 'mdi:bookmark',
+    buttonClass: ''
+  },
+  offBookmark: {
+    icon: 'mdi:bookmark',
+    buttonClass: ''
+  },
+  onGood: {
+    icon: 'mdi:thumb-up',
+    buttonClass: ''
+  },
+  offGood: {
+    icon: 'mdi:thumb-up-outline',
+    buttonClass: ''
+  },
+  search: {
+    icon: 'mdi:magnify',
+    buttonClass: ''
+  },
+  home: {
+    icon: 'mdi:home-variant',
+    buttonClass: ''
+  },
+  dashboard: {
+    icon: 'mdi:view-dashboard',
+    buttonClass: ''
+  },
+  event: {
+    icon: 'mdi:party-popper',
+    buttonClass: ''
+  },
+  follower: {
+    icon: 'mdi:account-arrow-left',
+    buttonClass: ''
+  },
+  follow: {
+    icon: 'mdi:account-arrow-right',
+    buttonClass: ''
+  },
+  team: {
+    icon: 'mdi:account-group',
+    buttonClass: ''
+  },
+  teamCreate: {
+    icon: 'mdi:account-multiple-plus',
+    buttonClass: ''
+  },
+  organize: {
+    icon: 'mdi:calendar',
+    buttonClass: ''
+  },
+  socialInteraction: {
+    icon: 'mdi:contacts',
+    buttonClass: ''
+  },
+  account: {
+    icon: 'mdi:shield-account',
+    buttonClass: ''
+  },
+  recruiting: {
+    icon: 'mdi:account-reactivate',
+    buttonClass: ''
+  },
+  profile: {
+    icon: 'mdi:card-account-details',
+    buttonClass: ''
+  },
+  activity: {
+    icon: 'mdi:flash',
+    buttonClass: ''
+  },
+  setting: {
+    icon: 'mdi:cog',
+    buttonClass: ''
+  },
+
   default: {
     icon: '',
     buttonClass: ''
   }
-};
+}
