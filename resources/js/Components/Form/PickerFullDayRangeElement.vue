f715d4653f0c62cb53ef184de30ee5e8f9932a0f
sphere <sphere.stacking@gmail.com>
2024-01-26 01:21:24 +0900

    feat(特定日の日にち内で時間のスパンを取得Pickerの作成):

--- resources/js/Components/Form/PickerFullDayRangeElement.vue
+++ resources/js/Components/Form/PickerFullDayRangeElement.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { format, startOfDay, addMinutes, set, differenceInMinutes } from 'date-fns'
+import { format, startOfDay, set, differenceInMinutes } from 'date-fns'
 import Slider from '@vueform/slider'
 
-defineProps({
+const emit = defineEmits(['update:modelValue'])
+const props = defineProps({
   id: {
     type: String,
     default: ''
@@ -17,7 +18,7 @@ defineProps({
   },
   placeholder: {
     type: String,
-    default: ''
+    default: 'yyyy/MM/dd HH:mm - yyyy/MM/dd HH:mm'
   },
   help: {
     type: String,
@@ -28,37 +29,39 @@ defineProps({
     default: null
   },
 })
+// ステップのオプションを定義
+const stepOptions = [1, 5, 15, 60, 120]
+
+// 時間範囲を設定するためのヘルパーメソッド
+const timeRanges = [
+  { label: '08:00 - 18:00', start: 8, end: 18 },
+  { label: '21:00 - 24:00', start: 21, end: 24 }
+]
+
 const slideValue = ref([540, 2280])
 const step = ref(15)
-const sliderMax = ref(2880)
+const sliderMax = ref(2879)
 const tooltipMerge = ref(900)
-const dates = ref([
-  startOfDay(new Date()),
-  startOfDay(new Date())
-])
 
-// const emit = defineEmits(['update:modelValue'])
-// const updateValue = (e) => {
-//   // 子要素のmodelValueをe.target.valueでupdate
-//   emit('update:modelValue', e.target.value)
-// }
+// modelValueの検証関数
+const isValidModelValue = (value) => {
+  return Array.isArray(value) && value.length === 2 && value.every(date => date instanceof Date)
+}
+
+// modelValueが適切な形式であればそれを使用し、そうでなければデフォルトの値を使用します。
+const dates = ref(isValidModelValue(props.modelValue) ? props.modelValue : [])
 
 /* calendarの設定 */
 const textInputOptions = {
-  format: ['yyyy/MM/dd HH:mm', 'HH:mm']
-}
-const formatDate = (date) => {
-  if (dates.value.length != 2) {
-    return ''
-  }
-  const start = date[0]
-  const end = date[1]
-  if (!(start instanceof Date) || isNaN(start.getTime()) || !(end instanceof Date) || isNaN(end.getTime())) {
-    return ''
-  }
-  return `${format(start, 'yyyy/MM/dd HH:mm')} - ${format(end, 'HH:mm')}`
+  format: ['yyyy/MM/dd HH:mm', 'yyyy/MM/dd HH:mm']
 }
 
+const formatDate = (date) => {
+  if (!Array.isArray(date) || date.length !== 2) {
+    return ''
+  }
+  return `${format(date[0], 'yyyy/MM/dd HH:mm')} - ${format(date[1], 'yyyy/MM/dd HH:mm')}`
+}
 /* スライダー */
 const SliderTooltip = (value) => {
   const hours = Math.floor(value / 60).toString().padStart(2, '0')
@@ -66,23 +69,22 @@ const SliderTooltip = (value) => {
   return `${hours} : ${minutes}`
 }
 
+const adjustSlideValue = (index, direction) => {
+  const newValue = slideValue.value[index] + step.value * direction
+  // スライダーの最小値と最大値を超えないように制限をかける
+  slideValue.value[index] = Math.max(0, Math.min(newValue, sliderMax.value))
+}
+
 /* calendarのside contents */
-const incrementMonth = (selectDate) => {
+const changeMonth = (increment, selectDate) => {
   dates.value = dates.value.map(d => {
     const newDate = new Date(d)
-    newDate.setMonth(newDate.getMonth() + 1)
+    newDate.setMonth(newDate.getMonth() + (increment ? 1 : -1))
     return newDate
   })
   selectDate({ value: dates.value[0], current: true })
 }
-const decrementMonth = (selectDate) => {
-  dates.value = dates.value.map(d => {
-    const newDate = new Date(d)
-    newDate.setMonth(newDate.getMonth() - 1)
-    return newDate
-  })
-  selectDate({ value: dates.value[0], current: true })
-}
+
 const setTimeRange = (startHour, endHour) => {
   const startDate = set(new Date(), { hours: startHour, minutes: 0 })
   const endDate = set(new Date(), { hours: endHour, minutes: 0 })
@@ -94,12 +96,20 @@ const updateStep = (e) => {
   step.value = Number(e.target.value)
 }
 
-const shiftSlide = (index, direction) => {
-  const newValue = slideValue.value[index] + step.value * direction
-  // スライダーの最小値と最大値を超えないように制限をかける
-  slideValue.value[index] = Math.max(0, Math.min(newValue, sliderMax.value))
-}
+//watch
 
+watch(slideValue, (newValue) => {
+  dates.value = newValue.map(time => {
+    const hours = Math.floor(time / 60)
+    const minutes = time % 60
+    return set(new Date(), { hours, minutes })
+  })
+})
+
+watch(dates, (newDates) => {
+  if (!isValidModelValue(newDates)) { return }
+  emit('update:modelValue', newDates)
+})
 </script>
 
 <template>
@@ -109,11 +119,18 @@ const shiftSlide = (index, direction) => {
       locale="ja"
       :format="formatDate"
       :text-input="textInputOptions"
+      :placeholder="placeholder"
       range
       auto-range="0"
       over
       dark
       @internal-model-change="handleInternal">
+      <template #dp-input="{ value }">
+        <div class="relative">
+          <Icon icon="mdi:calendar-range" class=" absolute left-2 top-1.5 text-xl" />
+          <input type="text" :value="value" class="input input-sm w-full pl-8">
+        </div>
+      </template>
       <template #left-sidebar="{selectDate}">
         <div class="flex h-full flex-col justify-end gap-1 pb-2">
           <div class="h-5">
@@ -125,49 +142,33 @@ const shiftSlide = (index, direction) => {
           <div class="h-5">
             Month
           </div>
-          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="incrementMonth(selectDate)">
+          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="changeMonth(1,selectDate)">
             Next
           </button>
-          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="decrementMonth(selectDate)">
+          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="changeMonth(-1,selectDate)">
             Previous
           </button>
 
           <div class="h-5">
             Time
           </div>
-          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="setTimeRange(9, 38)">
-            init
-          </button>
-          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="setTimeRange(8, 18)">
-            08:00 - 18:00
-          </button>
-          <button class="btn btn-primary btn-xs w-28 rounded-sm" @click="setTimeRange(21, 24)">
-            21:00 - 24:00
+          <button
+            v-for="time in timeRanges" :key="time.label" class="btn btn-primary btn-xs w-28 rounded-sm"
+            @click="setTimeRange(time.start, time.end)">
+            {{ time.label }}
           </button>
 
           <div class="h-5">
             Time step
           </div>
           <select v-model="step" class="select select-xs w-full  rounded-sm py-0" @change="updateStep">
-            <option :value="1">
-              1
-            </option>
-            <option :value="5">
-              5
-            </option>
-            <option :value="15">
-              15
-            </option>
-            <option :value="60">
-              60
-            </option>
-            <option :value="120">
-              120
+            <option v-for="option in stepOptions" :key="option" :value="option">
+              {{ option }}
             </option>
           </select>
         </div>
       </template>
-      <template #time-picker="{ time, updateTime }">
+      <template #time-picker>
         <div class="mx-10 mb-2 mt-10">
           <Slider
             v-model="slideValue"
@@ -176,19 +177,19 @@ const shiftSlide = (index, direction) => {
             :merge="tooltipMerge"
             :step="step" />
           <div class=" mt-2 grid grid-cols-5 gap-1">
-            <button class="btn btn-primary btn-xs rounded-sm" @click="shiftSlide(0, -1)">
+            <button class="btn btn-primary btn-xs rounded-sm" @click="adjustSlideValue(0, -1)">
               <Icon icon="mdi:chevron-left" class="text-xl" />
             </button>
-            <button class="btn btn-primary btn-xs rounded-sm" @click="shiftSlide(0, 1)">
+            <button class="btn btn-primary btn-xs rounded-sm" @click="adjustSlideValue(0, 1)">
               <Icon icon="mdi:chevron-right" class="text-xl" />
             </button>
 
             <div></div>
 
-            <button class="btn btn-primary btn-xs  rounded-sm" @click="shiftSlide(1, -1)">
+            <button class="btn btn-primary btn-xs  rounded-sm" @click="adjustSlideValue(1, -1)">
               <Icon icon="mdi:chevron-left" class="text-xl" />
             </button>
-            <button class="btn btn-primary btn-xs   rounded-sm" @click="shiftSlide(1, 1)">
+            <button class="btn btn-primary btn-xs   rounded-sm" @click="adjustSlideValue(1, 1)">
               <Icon icon="mdi:chevron-right" class="text-xl" />
             </button>
           </div>
