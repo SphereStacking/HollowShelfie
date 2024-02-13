421c1b463dc5d7fc2bdae0757502427c58f568be
SphereStacking <sphere.stacking@gmail.com>
2024-02-14 00:05:58 +0900

    fix: カスタムidとタグをメイリサーチに追加

--- app/Models/CustomIdentifiable.php
+++ app/Models/CustomIdentifiable.php
@@ -4,15 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+use PhpParser\Node\Expr\FuncCall;
 
 class CustomIdentifiable extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = ['alias_name'];
 
     public function aliasable()
     {
-        return $this->morphTo();
+        return $this->morphTo('aliasable', 'identifiable_type', 'identifiable_id');
+    }
+
+    /**
+     * MeiliSearch 検索可能な配列に変換します。
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        return $this->only(
+            [
+                'alias_name',
+            ]
+        );
     }
 }
