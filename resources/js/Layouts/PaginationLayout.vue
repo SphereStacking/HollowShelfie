ac6cf01c77ed3cec84dcd931ce312493e32d6882
sphere <sphere.stacking@gmail.com>
2024-02-13 23:45:14 +0900

    feat: paginationのUIを統一にするためLayoutを作成

---
+++ resources/js/Layouts/PaginationLayout.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  pagination: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="mx-auto flex max-w-7xl flex-col gap-5">
+    <div class="flex flex-row  justify-between">
+      <div class="">
+        {{ pagination.to }} / {{ pagination.total }}
+      </div>
+      <div class=" flex flex-row gap-2">
+        <slot name="TopPaginationLeft"></slot>
+        <BtnPagination
+          :prev-page-url="pagination.prev_page_url" :next-page-url="pagination.links"
+          :links="pagination.links" />
+        <slot name="TopPaginationRight"></slot>
+      </div>
+    </div>
+    <slot></slot>
+    <div class="flex flex-row justify-center">
+      <slot name="BottomPaginationLeft"></slot>
+      <BtnPagination
+        :prev-page-url="pagination.prev_page_url" :next-page-url="pagination.links"
+        :links="pagination.links" />
+      <slot name="BottomPaginationRight"></slot>
+    </div>
+  </div>
+</template>
+<style lang="">
+
+</style>
--- resources/js/Pages/Search/Event.vue
+++ resources/js/Pages/Search/Event.vue
@@ -64,45 +64,31 @@ onMounted(() => {
         :statuses="statuses"
         :categories="categories" :tags="trendTags" @execute-search="executeSearch()" />
     </div>
-
-    <div class="mx-auto flex max-w-7xl flex-col gap-5">
-      <div class="flex flex-row  justify-between">
-        <div class="">
-          {{ events.pagination.to }} / {{ events.pagination.total }}
-        </div>
-        <div class=" flex flex-row gap-2">
-          <BtnPagination
-            :prev-page-url="props.events.pagination.prev_page_url" :next-page-url="props.events.pagination.links"
-            :links="events.pagination.links" />
-          <div class="dropdown dropdown-end ">
-            <div tabindex="0" class=" btn indicator  btn-neutral btn-sm w-16 px-2">
-              <Icon icon="mdi:sort" class="text-xl transition-all" />
-              <div v-if="order.icon" class="badge indicator-item badge-info">
-                <Icon :icon="order.icon" class=" text-sm " />
-              </div>
+    <PaginationLayout :pagination="events.pagination">
+      <template #TopPaginationRight>
+        <div class="dropdown dropdown-end ">
+          <div tabindex="0" class=" btn indicator  btn-neutral btn-sm w-16 px-2">
+            <Icon icon="mdi:sort" class="text-xl transition-all" />
+            <div v-if="order.icon" class="badge indicator-item badge-info">
+              <Icon :icon="order.icon" class=" text-sm " />
             </div>
-            <ul tabindex="0" class="menu dropdown-content z-[1] w-28 rounded-box bg-base-100 p-2 shadow">
-              <li v-for="(orderItem, index) in orderMaps" :key="index" @click="order = orderItem; executeSearch()">
-                <div>
-                  <Icon :icon="orderItem.icon" />
-                  {{ orderItem.label }}
-                </div>
-              </li>
-            </ul>
           </div>
+          <ul tabindex="0" class="menu dropdown-content z-[1] w-28 rounded-box bg-base-100 p-2 shadow">
+            <li v-for="(orderItem, index) in orderMaps" :key="index" @click="order = orderItem; executeSearch()">
+              <div>
+                <Icon :icon="orderItem.icon" />
+                {{ orderItem.label }}
+              </div>
+            </li>
+          </ul>
         </div>
-      </div>
+      </template>
       <div class="my-2 grid w-full grid-cols-2  gap-6 sm:grid-cols-3  md:grid-cols-4 xl:grid-cols-6">
         <CardEvent
           v-for="(item, index) in props.events.data" :key="index" :event="item"
           scroll-region />
       </div>
-      <div class="flex flex-row justify-center">
-        <BtnPagination
-          :prev-page-url="props.events.pagination.prev_page_url" :next-page-url="props.events.pagination.links"
-          :links="events.pagination.links" />
-      </div>
-    </div>
+    </PaginationLayout>
   </AppLayout>
 </template>
 
