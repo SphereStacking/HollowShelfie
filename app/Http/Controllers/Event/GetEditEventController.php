d87751cd243cb55bfa9089f864ddc8b32ed8c993
sphere <sphere.stacking@gmail.com>
2024-03-02 21:55:49 +0900

    feat: 操作権限を確認してviewを返す

--- app/Http/Controllers/Event/GetEditEventController.php
+++ app/Http/Controllers/Event/GetEditEventController.php
@@ -8,6 +8,7 @@ use Inertia\Response;
 use App\Models\Category;
 use App\Models\InstanceType;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\InstanceTypeResource;
 use App\Http\Resources\EventEditJsonResource;
@@ -16,6 +17,9 @@ class GetEditEventController extends Controller
 {
     public function __invoke(Event $event): Response
     {
+        if (!$event->canUserOperate(Auth::user())) {
+            throw new CannotOperateEventException();
+        }
         return Inertia::render('Event/Edit', [
             'categories'=> CategoryResource::collection(Category::all()),
             'instanceTypes'=> InstanceTypeResource::collection(InstanceType::all()),
