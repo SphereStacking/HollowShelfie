ad460dcd936c0dffcab133591b775b0b268f2bac
SphereStacking <sphere.stacking@gmail.com>
2024-03-02 21:41:12 +0900

    feat: エラーの標準pageを整備

---
+++ resources/js/Pages/Errors/404Error.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  message: {
+    type: String,
+    default: 'ページが見つかりませんでした。'
+  }
+})
+</script>
+
+<template>
+  <div class="flex min-h-screen items-center justify-center px-4 py-8">
+    <div class="flex w-full max-w-md flex-col space-y-8">
+      <h1 class="mb-4 text-center text-3xl font-bold">
+        404
+      </h1>
+      <p class="text-center text-lg ">
+        {{ message }}
+      </p>
+      <Link href="/" class="btn ml-4">
+        戻る
+      </Link>
+    </div>
+  </div>
+</template>
+
--- resources/js/Pages/Errors/429Error.vue
+++ resources/js/Pages/Errors/429Error.vue
@@ -1,15 +1,22 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  message: {
+    type: String,
+    default: 'リクエストが多すぎます。'
+  }
+})
 </script>
 
 <template>
   <div class="flex min-h-screen items-center justify-center px-4 py-8">
     <div class="flex w-full max-w-md flex-col space-y-8">
       <h1 class="mb-4 text-center text-3xl font-bold">
-        イベントが公開されていません
+        429
       </h1>
-      <p class="text-center text-lg text-gray-700">
-        このイベントはまだ公開されていません。
+      <p class="0 text-center text-lg">
+        {{ message }}
       </p>
       <Link href="/" class="btn ml-4">
         戻る
