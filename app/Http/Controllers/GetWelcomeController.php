223ec13e1f2d96179373aaef58494ed6ababc832
sphere <sphere.stacking@gmail.com>
2024-03-11 01:38:11 +0900

    feat: 開催中のイベントを返却するように

--- app/Http/Controllers/GetWelcomeController.php
+++ app/Http/Controllers/GetWelcomeController.php
@@ -2,20 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Inertia\Inertia;
+use App\Enums\EventStatus;
 use Illuminate\Http\Request;
 use App\Services\UserService;
+use App\Services\EventService;
+use App\Http\Resources\EventsJsonResource;
 use App\Http\Resources\UserPublicProfileJsonResource;
-use App\Models\User;
 
 class GetWelcomeController extends Controller
 {
     protected $userService;
+    protected $eventService;
 
     public function __construct(
         UserService $userService,
+        EventService $eventService,
     ) {
         $this->userService = $userService;
+        $this->eventService = $eventService;
     }
 
     public function __invoke()
@@ -24,6 +30,7 @@ class GetWelcomeController extends Controller
             'user' => new UserPublicProfileJsonResource(
                 $this->userService->preloadProfileData(User::find(1)),
             ),
+            'events' => new EventsJsonResource($this->eventService->getPublicRandomEvents(15, [EventStatus::ONGOING])),
         ]);
     }
 }
