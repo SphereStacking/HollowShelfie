f52982f3692aa65fc8e91234b3e9a76b44d7cd8c
sphere <sphere.stacking@gmail.com>
2024-03-11 01:39:54 +0900

    feat: レイアウトを変更

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,105 +6,111 @@ import { usePage, Link } from '@inertiajs/vue3'
 const page = usePage()
 const appName = page.props.config.appName ?? null
 const user = page.props.user ?? null
+const events = page.props.events ?? []
 
 </script>
 
 <template>
-  <AppLayout title="Welcome">
-    <template #header>
-      <h2 class="text-xl font-semibold  leading-tight">
-        Welcome
-      </h2>
-    </template>
-
-    <div class="py-12">
-      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-          <Card>
-            <template #title>
-              <ApplicationLogo class="block h-12" />
-            </template>
-
-            <h1 class="text-2xl font-medium">
-              {{ appName }} へようこそ！
-            </h1>
-
-            <div class="mt-6 ">
-              <p class="text-xl font-bold">
-                VRコミュニティをよりアクセスしやすく
-              </p>
-              <p> VR世界は現在、コミュニティが分断されていると感じています。</p>
-              <br>
-              <p class="font-bold">
-                初めての出会いでその後が大きく左右します。
-              </p>
-
-              自分から探しに行かないとなかなか新しい体験を見つけるのは難しい。。<br>
-
-              <br>
-              演者、グループによるイベントの現在、過去、未来のイベントを簡単に探索し<br>
-              異なるコミュニティ外の繋がりを見つけやすく。
-
-              せっかく作成したフライヤーも一度きりで終わらせたく無い
-              <br>
-
-              <br>
-              そんな思いで<span class="font-bold">ユーザー投稿型プラットフォーム</span>の開発を進めています。<br>
-            </div>
-          </Card>
-
-          <Card>
-            <template #title>
-              <p class="text-2xl">
-                パトロンの募集
-              </p>
-            </template>
-            <div class="p-4">
-              <div class="mb-4">
-                <h2 class="text-lg font-bold">
-                  基本 無料
-                </h2>
-                <p>イベントの検索や閲覧やブックマーク等の基本的な機能は無料です</p>
-              </div>
-              <div class="mb-4">
-                <h2 class="text-lg font-bold">
-                  支援 300円
-                </h2>
-                <p>開発を継続していくための運営・開発・サーバー費用に充てさせていただきます。</p>
-              </div>
-              <div class="mb-4">
-                <h2 class="text-lg font-bold">
-                  団体 500円
-                </h2>
-                <p>Team機能の解放 ユーザー制限x5 </p>
-              </div>
-              <div class="mb-4">
-                <h2 class="text-lg font-bold">
-                  個人/団体 3000円
-                </h2>
-                <p>翌月からスポンサーとして広告を掲載することができます。</p>
-              </div>
-              <div class="mb-4">
-                <h2 class="text-lg font-bold">
-                  企業 要調整
-                </h2>
-                <p>翌月からスポンサーとして広告を掲載することができます。</p>
-              </div>
-            </div>
-          </Card>
-
-          <Card class="col-span-1 sm:col-span-2">
-            <template #title>
-              <p class="text-2xl">
-                開発者について
-              </p>
-            </template>
-            <div>
-              <img :src="user.dataile.photo_url" class="h-40 rounded-md">
-            </div>
-          </Card>
+  <AppLayout>
+    <section class="relative mx-auto w-full max-w-screen-lg overflow-x-clip px-5 pt-20">
+      <div class="flex flex-col items-center">
+        <ApplicationLogo class=" h-24" />
+      </div>
+      <div class="mt-20 flex flex-row items-center justify-around">
+        <div class=" items-center text-2xl font-bold">
+          <div>
+            新しい出会い、感動、そして冒険。<br>
+            あなたの次の体験を、ここから見つけて始めよう。<br>
+            <br>
+            未知なるイベントの世界へ一緒に踏み出しましょう。
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <Link href="/home" class="btn btn-primary btn-lg ">
+            イベントを探す
+          </Link>
+          <Link href="/home" class="btn btn-link btn-sm ">
+            今すぐ登録
+          </Link>
         </div>
       </div>
+    </section>
+    <section class="mt-20 overflow-hidden whitespace-nowrap">
+      <div class="inline-block animate-slide-infinite p-5">
+        <CardEvent
+          v-for="event in events" :key="event.id" :event="event"
+          class="m-5 inline-block w-40" />
+        <CardEvent
+          v-for="event in events" :key="event.id" :event="event"
+          class="m-5 inline-block w-40" />
+      </div>
+    </section>
+    <section class="mx-auto w-full max-w-screen-lg overflow-x-clip px-5 pt-20">
+      <div class="grid grid-cols-2 gap-1 rounded-md border-4 border-teal-300 p-2 text-base-content shadow-xl shadow-blue-500/50">
+        <div class="col-span-2 rounded-md p-4  text-center text-3xl font-black">
+          aaaaa
+          <br>
+          aa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+      </div>
+    </section>
+
+    <section class="mx-auto w-full max-w-screen-lg overflow-x-clip px-5 pt-20">
+      <div class="grid grid-cols-1 gap-1">
+        <div class="h-52 bg-base-300/10 p-4">
+          イベントをいいねで応援！
+        </div>
+        <div class="h-52 bg-base-300/20  p-4">
+          行きたいイベントをブックマーク！
+        </div>
+        <div class="h-52 bg-base-300/30  p-4">
+          主催したイベント掲載して告知しよう！
+        </div>
+        <div class="h-52 bg-base-300/40  p-4">
+          推しのオーガナイザー&パフォーマーをフォローしてイベントを見逃さない！
+        </div>
+      </div>
+    </section>
+    <section class="mx-auto  w-full max-w-screen-lg overflow-x-clip px-5 pt-20">
+      <div class="grid grid-cols-3 gap-1 rounded-md  border-4 border-pink-300 p-2 text-base-content shadow-xl shadow-pink-500/50">
+        <div class="col-span-3 rounded-md p-4 text-center text-3xl font-black">
+          Our Premium<br>
+          Sponsors
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+        <div class="rounded-md border-2 border-gray-300 bg-base-100/40 p-4">
+          aaaaa
+        </div>
+      </div>
+    </section>
+    <div>
+      <img :src="user.dataile.photo_url" class="h-40 rounded-md">
     </div>
   </AppLayout>
 </template>
