e402f7c767ce30a8314680e37eccd17468447331
SphereStacking <sphere.stacking@gmail.com>
2024-03-20 19:49:28 +0900

    refactor: コンポーネントの挙動を修正

--- resources/js/Components/TransitionInViewportObserver.vue
+++ resources/js/Components/TransitionInViewportObserver.vue
@@ -1,33 +1,49 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, defineProps } from 'vue'
 
 const isVisible = ref(false)
-const beforeRef = ref(null) // beforeRefを追加
-const afterRef = ref(null) // afterRefを追加
+const containerRef = ref(null)
+
+const props = defineProps({
+  // IntersectionObserverのオプション用のprops
+  observerOptions: {
+    type: Object,
+    default: () => ({
+      root: null,
+      rootMargin: '-20px 0px -20px 0px',
+      threshold: 0.5
+    })
+  }
+})
 
 onMounted(() => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       isVisible.value = entry.isIntersecting
     })
-  }, {
-    threshold: 0.1
-  })
+  }, props.observerOptions)
 
-  watch([beforeRef, afterRef], ([newBeforeRef, newAfterRef]) => {
-    if (newBeforeRef) observer.observe(newBeforeRef)
-    if (newAfterRef) observer.observe(newAfterRef)
+  watch([containerRef], ([newContainerRef]) => {
+    if (newContainerRef) observer.observe(newContainerRef)
   }, { immediate: true })
+})
+watch(isVisible, async (newVal) => {
+  if (newVal) {
+    await nextTick()
+    let contentHeight = 0
+    for (const child of containerRef.value.children) {
+      contentHeight += child.offsetHeight
+    }
+    containerRef.value.style.height = `${contentHeight}px`
+  }
 })
 </script>
 
 <template>
-  <div>
-    <span ref="beforeRef"></span>
+  <div ref="containerRef">
+    <span></span>
     <transition v-bind="$attrs">
-      <!-- トランジションを適用するための単一のラッパー要素を追加 -->
       <slot v-if="isVisible"></slot>
     </transition>
-    <span ref="afterRef"></span>
   </div>
 </template>
