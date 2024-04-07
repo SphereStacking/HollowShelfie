6904b4132c7cffb46092d1b057a95d9406dbf11f
sphere <sphere.stacking@gmail.com>
2024-04-07 20:28:47 +0900

    refactor: 外部キー制約

--- database/migrations/2020_12_22_000000_create_connected_accounts_table.php
+++ database/migrations/2020_12_22_000000_create_connected_accounts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('connected_accounts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('provider');
             $table->string('provider_id');
             $table->string('name')->nullable();
