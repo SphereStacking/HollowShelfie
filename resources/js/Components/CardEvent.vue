b4bf04a5b82f719890485ed48074083a2c308c8f
sphere <sphere.stacking@gmail.com>
2024-04-08 22:48:23 +0900

    feat: デザイン調整

--- resources/js/Components/CardEvent.vue
+++ resources/js/Components/CardEvent.vue
@@ -13,48 +13,57 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class=" card card-compact border border-transparent bg-base-200 shadow-xl shadow-blue-500/50 transition-all duration-200 hover:-translate-y-1 hover:border-base-300 hover:bg-base-100 hover:shadow-green-300/50  ">
-    <div class="mx-2 mt-2 flex justify-between">
-      <div class="badge badge-primary">
-        {{ event.category_name }}
-      </div>
-      <div class="truncate whitespace-nowrap text-xs  font-bold">
-        {{ event.event_timeline_status }}
-      </div>
-    </div>
-
-    <div class="mx-3 mt-1 truncate whitespace-nowrap font-bold ">
-      {{ event.title }}
-    </div>
-    <div class="relative aspect-a4 min-w-40">
+  <div class=" flex flex-col justify-end border border-transparent">
+    <div class="relative  min-w-40 bg-base-300">
       <template v-if="event.files.length>0">
         <Carousel
           :autoplay="5000" wrap-around class=""
           pause-autoplay-on-hover>
-          <Slide v-for="(image, index) in event.files" :key="index">
-            <img class="carousel__item h-full w-full object-cover" :src="image.public_url">
+          <Slide v-for="(image, index) in event.files" :key="index" class="aspect-a4">
+            <img class="carousel__item " :src="image.public_url">
           </Slide>
         </Carousel>
       </template>
       <template v-else>
         <div class="h-full w-full bg-base-300">
-          <Icon icon="mdi:image-off" class=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  text-6xl" />
+          <Icon icon="mdi:image-off" class=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl" />
         </div>
       </template>
     </div>
-    <div class="m-2 flex justify-between">
-      <div class="flex gap-1">
-        <BtnSwapEventBookmark :event-id="event.alias" :check="event.auth_user?.is_bookmark" />
-        <BtnSwapEventGood
-          :event-id="event.alias" :check="event.auth_user?.is_good" :count="event.short_good_count"
-          show-count />
-      </div>
-      <div></div>
-      <Link :href="route('event.show', event.alias)" class="text-md btn btn-link btn-active btn-xs m-0 p-0">
-        Read More
+
+    <div class="flex flex-row justify-between">
+      <Link :href="route('event.show', event.alias)" class="mx-2 truncate whitespace-nowrap font-bold ">
+        {{ event.title }}
       </Link>
-      <div></div>
+
+      <div class="dropdown dropdown-end dropdown-top">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-xs p-0">
+          <Icon icon="mdi:dots-vertical" class="text-xl" />
+        </div>
+        <ul tabindex="0" class="menu dropdown-content z-[1] w-28 rounded-box bg-base-200 p-2 shadow">
+          <li>
+            <div>
+              <BtnSwapEventBookmark :event-id="event.alias" :check="event.auth_user?.is_bookmark" />
+            </div>
+          </li>
+          <li>
+            <div>
+              <BtnSwapEventGood
+                :event-id="event.alias" :check="event.auth_user?.is_good" :count="event.short_good_count"
+                show-count />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="mx-2 flex justify-between">
+      <div>
+        {{ event.category_name }}
+      </div>
+      <div class="truncate whitespace-nowrap text-xs font-bold">
+        {{ event.event_timeline_status }}
+      </div>
     </div>
   </div>
 </template>
