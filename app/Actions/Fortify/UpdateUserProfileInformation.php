3d92487f43cbf510db9cc51f1f77022c0fb7b8d9
sphere <sphere.stacking@gmail.com>
2024-04-14 01:38:03 +0900

    feat: vendorのファイルはphpstan対象外にしたため元に戻す

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -2,8 +2,8 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
