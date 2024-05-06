81b7a2750b7214a296fff073ee5d8d99b12ddb53
SphereStacking <sphere.stacking@gmail.com>
2024-05-06 16:58:45 +0900

    feat: headerが上に固定されるようにする

--- resources/js/Jetstream/ApplicationLogo.vue
+++ resources/js/Jetstream/ApplicationLogo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-row items-center gap-2 text-7xl">
-    <ApplicationMark class="h-32 w-32 border-2 border-neutral-800" />
+  <div class="flex flex-row items-center gap-4 text-7xl">
+    <ApplicationMark class="h-32 w-32" />
     <div class=" font-title font-black">
       Ghost Shelfie
     </div>
