a7ef4622d8276baba7626e27e7ea79b5e43ec59e
SphereStacking <sphere.stacking@gmail.com>
2024-05-19 00:10:20 +0900

    feat: 権限がない場合のmessageをDBから返すように

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -3,8 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\Permission;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class PermissionMiddleware
@@ -14,11 +14,14 @@ class PermissionMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $permission = null): Response
+    public function handle(Request $request, Closure $next, string $permission): Response
     {
-        Log::info('PermissionMiddleware::handle()');
-        if ($permission !== null && !$request->user()->can($permission)) {
-            abort(403);
+        $permissionRecord = Permission::where('name', $permission)->first();
+        if (!$permissionRecord) {
+            abort(500, '権限が見つかりません。');
+        }
+        if (!$request->user()->can($permission)) {
+            abort(403, $permissionRecord->permission_denied_message);
         }
 
         return $next($request);
--- database/migrations/2024_05_18_113639_create_permissions_table.php
+++ database/migrations/2024_05_18_113639_create_permissions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
+            $table->text('permission_denied_message')->nullable();
         });
     }
 
