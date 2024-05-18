23109f3a789b447e091c3e58e0d68db8fdecb317
SphereStacking <sphere.stacking@gmail.com>
2024-05-19 02:23:15 +0900

    fix: laravelの命名規則に合わせる

--- database/migrations/2024_05_18_113729_create_role_permission_table.php
+++ database/migrations/2024_05_18_113729_create_role_permission_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role_permission', function (Blueprint $table) {
+        Schema::create('permission_role', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role_permission');
+        Schema::dropIfExists('permission_role');
     }
 };
