fa6f698aafe2d0dc0415208238c489e70fc50a33
SphereStacking <sphere.stacking@gmail.com>
2024-05-20 01:16:09 +0900

    feat: dashboardの整理

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -24,15 +24,15 @@ const dashboardItems = [
   { category: 'activity', iconType: 'bookmarks', title: 'bookmarks', url: route('user.bookmark', props.auth.user.screen_name), buttonText: props.counts.bookmark?.total || 0 },
   { category: 'activity', iconType: 'onGood', title: 'Goods', url: route('user.good', props.auth.user.screen_name), buttonText: props.counts.good || 0 },
   { category: 'search', iconType: 'event', title: 'イベントを探す', url: route('event.search.index'), buttonText: 'go' },
-  { category: 'search', iconType: 'team', title: 'チームを探す(仮)', url: '#', buttonText: 'go' },
-  { category: 'search', iconType: 'user', title: 'ユーザーを探す(仮)', url: '#', buttonText: 'go' },
-  { category: 'search', iconType: 'recruiting', title: '募集を探す(仮)', url: route('event.recruiting'), buttonText: 'go' },
+  // { category: 'search', iconType: 'team', title: 'チームを探す(仮)', url: '#', buttonText: 'go' },
+  // { category: 'search', iconType: 'user', title: 'ユーザーを探す(仮)', url: '#', buttonText: 'go' },
+  // { category: 'search', iconType: 'recruiting', title: '募集を探す(仮)', url: route('event.recruiting'), buttonText: 'go' },
   { category: 'socialInteraction', iconType: 'follow', title: 'フォロー', url: route('user.following', props.auth.user.screen_name), buttonText: props.counts.follow || 0 },
   { category: 'socialInteraction', iconType: 'follower', title: 'フォロワー', url: route('user.follower', props.auth.user.screen_name), buttonText: props.counts.follower || 0 },
   { category: 'team', iconType: 'teamCreate', title: 'チームを作成する', url: route('teams.create'), buttonText: 'go!' },
-  { category: 'team', iconType: 'team', title: '所属チーム(仮)', url: '#', buttonText: 'go' },
-  { category: 'organize', iconType: 'event', title: 'イベントを主催する(仮)', url: route('event.create'), buttonText: 'go' },
-  { category: 'organize', iconType: 'myEvents', title: 'イベント管理(仮)', url: route('event.manage'), buttonText: '表示' },
+  // { category: 'team', iconType: 'team', title: '所属チーム(仮)', url: '#', buttonText: 'go' },
+  { category: 'organize', iconType: 'event', title: 'イベントを主催する', url: route('event.create'), buttonText: 'go' },
+  { category: 'organize', iconType: 'myEvents', title: 'イベント管理', url: route('event.manage'), buttonText: '表示' },
   { category: 'setting', iconType: 'account', title: 'アカウント設定', url: route('profile.show'), buttonText: 'go' },
   { category: 'setting', iconType: 'profile', title: '公開プロファイル', url: props.auth.user.profile_url, buttonText: 'go' },
 ]
@@ -46,9 +46,9 @@ const dashboardItems = [
       </h2>
     </template>
 
-    <div class="mx-auto grid w-full max-w-7xl grid-cols-2  gap-5 sm:grid-cols-3  md:grid-cols-4 xl:grid-cols-6">
+    <div class="mx-auto mb-10 grid w-full max-w-7xl  grid-cols-2 gap-5 sm:grid-cols-3  md:grid-cols-4 xl:grid-cols-6">
       <template v-for="category in dashboardCategory" :key="category.id">
-        <div class="divider divider-start col-span-full mt-10 text-xl  font-semibold">
+        <div class="divider divider-start col-span-full text-xl font-semibold">
           <IconTypeMapper :type="category.iconType" class="text-2xl" />
           {{ category.title }}
         </div>
