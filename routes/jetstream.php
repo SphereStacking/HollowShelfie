d7f5763c5d53cc4f2d637d41baefac006b70b439
SphereStacking <sphere.stacking@gmail.com>
2024-05-19 13:24:34 +0900

    feat: チーム関連のrouteに権限を追加

--- routes/jetstream.php
+++ routes/jetstream.php
@@ -55,22 +55,24 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
 
             // Teams...
             if (Jetstream::hasTeamFeatures()) {
-                Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
-                Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
-                Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
-                Route::put('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
-                Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
-                Route::put('/current-team', [CurrentTeamController::class, 'update'])->name('current-team.update');
-                Route::post('/teams/{team}/members', [TeamMemberController::class, 'store'])->name('team-members.store');
-                Route::put('/teams/{team}/members/{user}', [TeamMemberController::class, 'update'])->name('team-members.update');
-                Route::delete('/teams/{team}/members/{user}', [TeamMemberController::class, 'destroy'])->name('team-members.destroy');
+                Route::group(['middleware' => 'permission:team-management'], function () {
+                    Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
+                    Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
+                    Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
+                    Route::put('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
+                    Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
+                    Route::put('/current-team', [CurrentTeamController::class, 'update'])->name('current-team.update');
+                    Route::post('/teams/{team}/members', [TeamMemberController::class, 'store'])->name('team-members.store');
+                    Route::put('/teams/{team}/members/{user}', [TeamMemberController::class, 'update'])->name('team-members.update');
+                    Route::delete('/teams/{team}/members/{user}', [TeamMemberController::class, 'destroy'])->name('team-members.destroy');
 
-                Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
-                    ->middleware(['signed'])
-                    ->name('team-invitations.accept');
+                    Route::get('/team-invitations/{invitation}', [TeamInvitationController::class, 'accept'])
+                        ->middleware(['signed'])
+                        ->name('team-invitations.accept');
 
-                Route::delete('/team-invitations/{invitation}', [TeamInvitationController::class, 'destroy'])
-                    ->name('team-invitations.destroy');
+                    Route::delete('/team-invitations/{invitation}', [TeamInvitationController::class, 'destroy'])
+                        ->name('team-invitations.destroy');
+                });
             }
         });
     });
