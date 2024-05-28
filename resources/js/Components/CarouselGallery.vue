90f916a64e63e730d455f914589748c5cbd6e68c
SphereStacking <sphere.stacking@gmail.com>
2024-05-29 00:56:33 +0900

    feat: vue3-carouselバージョンをあげる

--- resources/js/Components/CarouselGallery.vue
+++ resources/js/Components/CarouselGallery.vue
@@ -1,7 +1,6 @@
 <script setup>
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel'
 
 const props = defineProps({
   images: {
