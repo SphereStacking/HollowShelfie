21d97f036a790b66c60321d9288efda08384f785
SphereStacking <sphere.stacking@gmail.com>
2024-06-10 00:49:49 +0900

    feat: サポーターを追加

--- config/sponsors.php
+++ config/sponsors.php
@@ -5,6 +5,12 @@ return [
         [
             'name' => 'Saina',
             'image' => 'storage/sponsor/Saina.png',
+            'url' => 'https://x.com/SainaKey',
+        ],
+        [
+            'name' => 'COMA.',
+            'image' => 'storage/sponsor/COMA.png',
+            'url' => 'https://x.com/ComaPre',
         ],
     ],
 ];
