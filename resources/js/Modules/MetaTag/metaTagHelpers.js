bb6d8da724972c997f22676c76f08709a4c63a77
SphereStacking <sphere.stacking@gmail.com>
2024-06-09 19:11:12 +0900

    fix: サニタイズを追加

--- resources/js/Modules/MetaTag/metaTagHelpers.js
+++ resources/js/Modules/MetaTag/metaTagHelpers.js
@@ -1,24 +1,25 @@
 import { usePage } from '@inertiajs/vue3'
+import DOMPurify from 'isomorphic-dompurify'
 
 export function GetAppMetaTags() {
   return [
     { property: 'og:type', content: 'website' },
-    { property: 'og:title', content: usePage().props.title },
-    { property: 'og:description', content: usePage().props.description },
-    { property: 'og:site_name', content: usePage().props.config.appName },
+    { property: 'og:title', content: DOMPurify.sanitize(usePage().props.title) },
+    { property: 'og:description', content: DOMPurify.sanitize(usePage().props.description) },
+    { property: 'og:site_name', content: DOMPurify.sanitize(usePage().props.config.appName) },
     { property: 'og:image', content: `${usePage().props.ziggy.url}/storage/images/App/Logo.svg` },
   ]
 }
 
 export function GetEventDetailMetaTags() {
   const eventMetaTags = [
-    { property: 'og:url', content: usePage().props.ziggy.location },
+    { property: 'og:url', content: DOMPurify.sanitize(usePage().props.ziggy.location) },
     { property: 'og:type', content: 'article' },
-    { property: 'og:title', content: usePage().props.event.title },
-    { property: 'og:description', content: usePage().props.event.description },
-    { property: 'og:site_name', content: usePage().props.config.appName },
-    { property: 'article:published_time', content: usePage().props.event.start_date },
-    { property: 'article:modified_time', content: usePage().props.event.end_date },
+    { property: 'og:title', content: DOMPurify.sanitize(usePage().props.event.title) },
+    { property: 'og:description', content: DOMPurify.sanitize(usePage().props.event.description) },
+    { property: 'og:site_name', content: DOMPurify.sanitize(usePage().props.config.appName) },
+    { property: 'article:published_time', content: DOMPurify.sanitize(usePage().props.event.start_date) },
+    { property: 'article:modified_time', content: DOMPurify.sanitize(usePage().props.event.end_date) },
   ]
   usePage().props.event.files.map((file) => {
     eventMetaTags.push({ property: 'og:image', content: file.public_url })
