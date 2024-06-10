d6f4edfef5861d1ac2a063686c659b28662302b2
SphereStacking <sphere.stacking@gmail.com>
2024-06-11 02:28:17 +0900

    feat: SynapseRackの広告追加

--- resources/js/Components/AreaAdvertisementRecruitment.vue
+++ resources/js/Components/AreaAdvertisementRecruitment.vue
@@ -1,13 +1,29 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  link: {
+    type: String,
+    default: () => route('about', 'advertisement'),
+  },
+  as: {
+    type: String,
+    default: Link,
+  },
+})
 </script>
 
 <template>
-  <Link
-    :href="route('about', 'advertisement')"
-    class="flex flex-row items-center justify-center rounded-lg border-2 border-dashed border-neutral bg-base-300 hover:bg-base-300/80">
-    <div class="flex flex-row  p-2">
-      広告募集！
+  <component
+    :is="as" :href="link" target="_blank"
+    class=" relative flex flex-row items-center justify-center rounded-lg border-2 border-dashed border-neutral bg-base-300 hover:bg-base-300/80">
+    <div class="absolute bottom-0 right-0  bg-accent px-2 opacity-70">
+      <span class=" text-sm font-bold  text-accent-content">広告</span>
     </div>
-  </Link>
+    <slot>
+      <div class="flex flex-row  p-2">
+        広告募集！
+      </div>
+    </slot>
+  </component>
 </template>
