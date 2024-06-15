162b5ad4bd98d1692fbbf5dd9e808f7862a9f188
SphereStacking <sphere.stacking@gmail.com>
2024-06-15 21:13:08 +0900

    feat: lp-update
    導線の追加と各セクションを調整

--- resources/js/Jetstream/ApplicationLogo.vue
+++ resources/js/Jetstream/ApplicationLogo.vue
@@ -9,10 +9,13 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex h-full items-center gap-4 " :class="isResponsive ? 'md:flex-row' : 'flex-col'">
-    <ApplicationMark class="size-32" />
-    <div class=" font-title text-4xl font-black sm:text-7xl">
-      <AppName />
+  <div class="flex h-full items-center" :class="isResponsive ? 'md:flex-row' : 'flex-col'">
+    <div class="flex flex-col items-center gap-2 px-4">
+      <ApplicationMark class="size-32" />
+      <div class="font-title text-xl font-black  sm:text-3xl " :class="isResponsive ? 'text-center' : ''">
+        Hollow
+      </div>
     </div>
+    <span class="font-title text-4xl font-black sm:text-8xl">Shelfie</span>
   </div>
 </template>
