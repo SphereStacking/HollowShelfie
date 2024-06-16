f98d0d2d25064eb9de9c9ecf5af05e110ebddb9f
SphereStacking <sphere.stacking@gmail.com>
2024-06-16 23:43:33 +0900

    build: 本番環境ではminifyするように

--- vite.config.ts
+++ vite.config.ts
@@ -4,46 +4,50 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: 'resources/js/app.js',
-      ssr: 'resources/js/ssr.js',
-      refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
+export default defineConfig(({mode})=> {
+  return {
+    plugins: [
+      laravel({
+        input: 'resources/js/app.js',
+        ssr: 'resources/js/ssr.js',
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
         },
-      },
-    }),
-    AutoImport({
-      imports: ['vue'],
-      dts: 'resources/js/types/auto-imports.d.ts',
-      eslintrc: {
-        enabled: true,
-      },
-    }),
-    Components({
-      dirs: ['resources/js'],
-      dts: 'resources/js/types/components.d.ts',
-      resolvers: [
-        (name) => {
-          if (name == 'Icon')
-            return { name, from: '@iconify/vue' }
+      }),
+      AutoImport({
+        imports: ['vue'],
+        dts: 'resources/js/types/auto-imports.d.ts',
+        eslintrc: {
+          enabled: true,
         },
-      ],
-    }),
-
-  ],
-  server: {
-    hmr: {
-      host: 'localhost',
+      }),
+      Components({
+        dirs: ['resources/js'],
+        dts: 'resources/js/types/components.d.ts',
+        resolvers: [
+          (name) => {
+            if (name == 'Icon')
+              return { name, from: '@iconify/vue' }
+          },
+        ],
+      }),
+    ],
+    build: {
+      minify: mode === 'production' ? 'esbuild' : false,
     },
-    watch: {
-      usePolling: true
+    server: {
+      hmr: {
+        host: 'localhost',
+      },
+      watch: {
+        usePolling: true
+      }
     }
   }
 })
