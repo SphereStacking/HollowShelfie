3133509759abfcc8398102a026faa0aa5f1d669b
SphereStacking <sphere.stacking@gmail.com>
2024-06-22 19:28:40 +0900

    fix: ssrでの遷移エラー対応

--- resources/js/Pages/Event/Partials/ShowDescription.vue
+++ resources/js/Pages/Event/Partials/ShowDescription.vue
@@ -21,7 +21,7 @@ onMounted(() => {
   <div class="flex w-full flex-col  justify-center">
     <div class="rounded-xl bg-base-200 p-5">
       <!-- eslint-disable-next-line vue/no-v-html -->
-      <article class=" prose h-full  overflow-y-auto" v-html="sanitizedDescription"></article>
+      <article class="prose h-full  overflow-y-auto" v-html="description"></article>
     </div>
   </div>
 </template>
