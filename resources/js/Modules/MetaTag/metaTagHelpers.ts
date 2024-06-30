30f49a07ea329a2c7559ebcbdec6e35ac880d826
SphereStacking <sphere.stacking@gmail.com>
2024-07-01 02:59:21 +0900

    feat: title,description,keywordsを追加

--- resources/js/Modules/MetaTag/metaTagHelpers.ts
+++ resources/js/Modules/MetaTag/metaTagHelpers.ts
@@ -8,22 +8,50 @@ export type MetaTag = {
   | { property: string; name?: never }
 )
 
+const title = 'VRChat イベントカレンダー | '
+const description = 'VRChatのイベントの雰囲気がフライヤーから一目で分かる！イベント情報を日別・週別・月別・タグ・カテゴリ・主催者・演者など様々な条件で簡単に検索・共有できるサービス。'
+const keywords = 'VRChat,Cluster,VR,イベント,カレンダー,バーチャルリアリティ,ソーシャルVR,イベントカレンダー,Event Calendar,メタバース,バーチャルイベント,オンラインイベント,VRコミュニティ,アバター,ワールド,バーチャルライブ'
+
 /**
  * アプリ全体で共通に使用するMetaTagsを取得します。
  * @returns {MetaTag[]} MetaTagsを返します。
  */
 export function GetAppMetaTags(): MetaTag[] {
+  const appName = usePage().props.config.appName
+  const url = usePage().props.ziggy.url
   return [
+    { name: 'title', content: title + appName },
+    { name: 'description', content: description },
+    { name: 'keywords', content: keywords },
     { property: 'og:type', content: 'website' },
-    { property: 'og:title', content: usePage().props.config.appName },
-    { property: 'og:description', content: 'イベントを探しやすく、共有しやすく。フライヤーを通じてイベントを告知しよう。' },
-    { property: 'og:site_name', content: usePage().props.config.appName },
-    { property: 'og:image', content: `${usePage().props.ziggy.url}/storage/images/ogp/summary_large_image.webp` },
+    { property: 'og:title', content: appName },
+    { property: 'og:description', content: description },
+    { property: 'og:site_name', content: appName },
+    { property: 'og:image', content: `${url}/storage/images/ogp/summary_large_image.webp` },
     { property: 'twitter:card', content: 'summary_large_image' },
-    { property: 'twitter:title', content: usePage().props.config.appName },
-    { property: 'twitter:description', content: 'イベントを探しやすく、共有しやすく。フライヤーを通じてイベントを告知しよう。' },
-    { property: 'twitter:image', content: `${usePage().props.ziggy.url}/storage/images/ogp/summary_large_image.webp` },
+    { property: 'twitter:title', content: appName },
+    { property: 'twitter:description', content: description },
+    { property: 'twitter:image', content: `${url}/storage/images/ogp/summary_large_image.webp` },
   ]
+}
+
+export function GetAppJsonLd(): string {
+  const appName = usePage().props.config.appName
+  const url = usePage().props.ziggy.url
+
+  return JSON.stringify({
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    'name': title + appName,
+    'url': url,
+    'description': description,
+    'potentialAction': {
+      '@type': 'SearchAction',
+      'target': `${url}/search?q={search_term_string}`,
+      'query-input': 'required name=search_term_string'
+    },
+    'keywords': keywords
+  })
 }
 
 /**
