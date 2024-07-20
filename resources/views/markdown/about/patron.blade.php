d57ad65c19404fb20c60b5e4a3e454ca43d9e1e9
SphereStacking <sphere.stacking@gmail.com>
2024-07-20 18:22:42 +0900

    feat: Patronのページをfooterに追加

---
+++ resources/views/markdown/about/patron.blade.php
@@ -0,0 +1,61 @@
+# **支援**について
+
+## 支援サイト
+
+発支援者の管理ができないため、支援特典は継続支援の方のみにさせていただきます。
+
+### 単発支援
+
+- [PayPal]({{ config('external_services.supportings.paypal') }})
+
+### 継続支援
+
+- [pixiv fanbox]({{ config('external_services.supportings.fanbox') }})
+- [Patreon]({{ config('external_services.supportings.patreon') }})
+
+#### 👻 プラン
+
+希望者には下記を提供致します。
+
+- LP(ランディングページ)の支援者欄への表示
+- HOMEでPickUP Eventerへの表示
+
+#### 👻👻 プラン
+
+希望者には下記を提供致します。
+
+- LP(ランディングページ)の支援者欄への表示
+- HOMEでPickUP Eventerへの表示
+- 試験運用機能を開放します。
+
+#### 👻👻👻 プラン
+
+希望者には下記を提供致します。
+
+- LP(ランディングページ)の支援者欄への表示
+- HOMEでPickUP Eventerへの表示
+- ランダムで広告枠への表示(画像とURL)
+- 試験運用機能を開放します。
+
+#### 👻👻👻👻 プラン
+
+開発スピードがめっちゃアップするプランです。希望者には下記を提供致します。
+
+- LP(ランディングページ)の支援者欄への表示
+- HOMEでPickUP Eventerへの表示
+- ランダムで広告枠への表示(画像とURL)
+- 試験運用機能を開放します。
+
+### 支援者特典について
+
+特典の受け取りは下記からお願いします。
+
+- pixiv fanbox [こちら]({{ config('external_services.supportings.fanbox') }})
+- Patreon [こちら]({{ config('external_services.supportings.patreon') }})
+
+※注意※
+
+解放される機能はあくまで**おまけ**程度にお考え下さい。
+
+本リリースや開発の際にデータ調整で
+データが消える可能性があることをご理解ください。
