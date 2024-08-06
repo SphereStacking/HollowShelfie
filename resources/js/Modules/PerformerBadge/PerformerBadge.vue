94360eaf2d850aa2baaa74b911e197251c57ce65
SphereStacking <sphere.stacking@gmail.com>
2024-08-07 06:44:55 +0900

    fix: PerformerBadgeテンプレート構造の更新
    - メインコンテナdivにflex-nowrapクラスを追加
    - 内部のdivをspanに変更し、セマンティックな構造を改善
    - テキスト要素が適切に切り捨てられ、整列されるように修正

--- resources/js/Modules/PerformerBadge/PerformerBadge.vue
+++ resources/js/Modules/PerformerBadge/PerformerBadge.vue
@@ -13,30 +13,30 @@ defineProps<Props>()
 
 <template>
   <div
-    class="btn btn-lg flex w-44 flex-row items-center justify-start gap-2 rounded-md p-2 text-sm"
+    class="btn btn-lg flex w-44 flex-row flex-nowrap items-center justify-start gap-2 rounded-md p-2 text-sm "
     :class="isSearching ? 'skeleton bg-base-200' : ''">
     <template v-if="performer.image_url">
       <img :src="performer.image_url" class="w-10 rounded-xl">
     </template>
     <template v-else-if="isSearching">
-      <div class=" rounded-xl bg-base-200">
+      <div class="rounded-xl bg-base-200">
         <div class="skeleton size-10"></div>
       </div>
     </template>
     <template v-else>
-      <div class=" rounded-xl bg-base-200">
+      <div class="rounded-xl bg-base-200">
         <IconTypeMapper type="unknownUser" class="skeleton size-10 p-2" />
       </div>
     </template>
-    <div class="flex flex-col justify-start text-left ">
-      <div class="truncate font-bold ">
+    <div class="flex flex-col justify-start truncate text-left">
+      <span class="truncate font-bold ">
         {{ performer.name }}
-      </div>
-      <div class="text-xs opacity-30">
+      </span>
+      <span class="text-xs opacity-30">
         <template v-if="performer.screen_name">
           @{{ performer.screen_name }}
         </template>
-      </div>
+      </span>
     </div>
   </div>
 </template>
